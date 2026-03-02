1d0349b82168e5a5e3eb96daafd9da7ef99aeacd
ewullmo2nd-spec <ewullmo2nd@gmail.com>
2026-03-03 04:46:00 +0800

    Update publicapp.js

--- dtr-systemfolder/public/publicapp.js
+++ dtr-systemfolder/public/publicapp.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://dtr-backend-tvwd.onrender.com/api";
 
 document.getElementById('current-date').innerText = new Date().toDateString();
 
